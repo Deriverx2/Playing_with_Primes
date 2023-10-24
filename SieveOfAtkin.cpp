ff8abf510c5ee4c8ea4697fa60cdd40104bdb95e
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-10-24 21:36:27 +0530

    Update SieveOfAtkin.cpp

--- SieveOfAtkin.cpp
+++ SieveOfAtkin.cpp
@@ -1,67 +1,62 @@
-// Algorithm to calculate prime numbers
 #include <iostream>
-#include <cmath>
 #include <vector>
-using std::cin, std::cout, std::endl, std::vector;
-using Int = unsigned int;
+#include <cmath>
 
-void Sieve(Int n)
-{
-    if (n >= 2)
-    {
-        cout << "2 ";
-    }
-    if (n >= 3)
-    {
-        cout << "3 ";
-    }
+using std::cin, std::cout, std::endl, std::vector;
+using Int = unsigned long long;
+
+void Sieve(Int n) {
+    // if (n >= 2) {
+    //     cout << "2 ";
+    // }
+    // if (n >= 3) {
+    //     cout << "3 ";
+    // }
     if (n < 5)
         return;
-    vector<bool> sieveList(++n, 0);
-    for (int i = 1; i * i < n; i++)
-    {
+    vector<bool> sieveList(n, false);  // Change here
+    int operations=0;
+    for (int i = 1; i * i < n; i++) {
         int guess;
         int x = i * i;
-        for (int j = 1; j * j < n; j++)
-        {
+        for (int j = 1; j * j < n; j++) {
+            operations++;
             int y = j * j;
             guess = (4 * x) + (y);
             if (guess <= n && (guess % 12 == 1 || guess % 12 == 5))
-                sieveList[guess] = 1;
+                sieveList[guess] = !sieveList[guess];
             guess = (3 * x) + (y);
             if (guess <= n && guess % 12 == 7)
-                sieveList[guess] = 1;
+                sieveList[guess] = !sieveList[guess];
             guess = (3 * x) - (y);
             if (i > j && guess <= n && guess % 12 == 11)
-                sieveList[guess] = 1;
+                sieveList[guess] = !sieveList[guess];
         }
     }
-    int r = 5;
-    while (r * r < n)
-    {
-        if (sieveList[r])
-            for (int j = r * r; j < n; j += r * r)
-            {
-                sieveList[j] = 0;
+
+    for (int r = 5; r * r <= n; r++) {
+        if (sieveList[r]) {
+            for (int i = r * r; i <= n; i += r * r) {
+                sieveList[i] = false;
+                operations++;
             }
-        r++;
+        }
     }
+    cout<<operations<<"+";
 
     // Printing primes from 5
-    for (int a = 5; a < n; a++)
-    {
-        if (sieveList[a])
-            cout << a << " ";
-    }
+    // for (int a = 5; a < n; a++) {
+    //     if (sieveList[a])
+    //         cout << a << " ";
+    // }
 }
 
-int main()
-{
+int main() {
     cout << "Calculate primes in a given range" << endl;
     Int n;
-    cout << "Enter a positive numbers: ";
+    cout << "Enter a positive number: ";
     cin >> n;
-    cout << "Prime numbers: ";
+    // cout << "Prime numbers:";
     Sieve(n);
     return 0;
 }
